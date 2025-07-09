2a6950ba328d54b52f6bff86983b41bc6899457a
JabDoesThings <JabDoesThings@github.com>
2025-07-09 15:20:21 -0500

    Creation of instanced inner class test for revise of scope subsystem.

---
+++ tests/class/test-class-instance-child.lua
@@ -0,0 +1,38 @@
+---[[
+--- @author asledgehammer, JabDoesThings 2025
+---]]
+
+local LuaClass = require 'LuaClass';
+local dump     = require 'dump'
+local packages = LuaClass.packages;
+
+print('## TEST ##\n');
+
+local builder = LuaClass.builder;
+local static = builder.static;
+local class = builder.class;
+local public = builder.public;
+
+
+-- Main files need to initialize LuaClass. --
+
+--- Java Example:
+--- ```java
+--- public class EnclosingClass {
+---   public static class EnclosedClass {}
+--- }
+--- ```
+local EnclosingClass = class 'EnclosingClass' (public) {
+  class 'EnclosedClass' (public) {
+
+  }
+};
+
+print('\n## TEST ##\n');
+
+print(EnclosingClass);
+print(EnclosingClass.EnclosedClass);
+print(packages);
+print(packages.tests);
+print(packages.tests.class);
+print(packages.tests.class.EnclosingClass.EnclosedClass);
