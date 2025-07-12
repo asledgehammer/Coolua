7d875e4b8048f99bf8c8ce294310e4a9b1b86c54
JabDoesThings <JabDoesThings@github.com>
2025-07-12 13:50:22 -0500

    Removed demos folder.

--- demos/demo-currentline.lua
+++
@@ -1,8 +0,0 @@
---- @param level integer
---- 
---- @return integer currentLine
-local function getCurrentLine(level)
-    return debug.getinfo(level, 'l').currentline;
-end
-
-print("Current line: ", getCurrentLine(2));
